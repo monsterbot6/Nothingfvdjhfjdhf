927c51deb87b09b2ec0a16bc016a43b321d55063
monsterbot6 <46303675+monsterbot6@users.noreply.github.com>
2019-07-10 20:18:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on("message", message => {//bc1
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(${argresult}\n ${m}\n By Server : ${message.guild.name});
+ m.send(${argresult}\n {m}\n By Server : ${message.guild.name});
 })
  message.channel.send(\${message.guild.members.filter(m => m.presence.status !== 'all').size}` : عدد الاعضاء المستلمين`); 
  message.delete(); 
